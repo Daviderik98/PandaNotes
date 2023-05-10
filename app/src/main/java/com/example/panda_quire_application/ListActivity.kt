9a556cb4c1ef091666e817411f1ee036cfaa1b31
Jonathan <storfot32@gmail.com>
2023-05-10 15:07:47 +0200

    List activity update

--- app/src/main/java/com/example/panda_quire_application/ListActivity.kt
+++ app/src/main/java/com/example/panda_quire_application/ListActivity.kt
@@ -2,10 +2,38 @@ package com.example.panda_quire_application
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
 
 class ListActivity : AppCompatActivity() {
+    private lateinit var notesList: MutableList<String>
+    private lateinit var notesListView: ListView
+    private lateinit var notesAdapter: ArrayAdapter<String>
+    private lateinit var notesInput: EditText
+    private lateinit var saveButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
+        notesList = mutableListOf()
+        notesListView = findViewById(R.id.notes_list)
+        notesInput = findViewById(R.id.notes_input)
+        saveButton  = findViewById(R.id.save_button)
+
+        notesAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, notesList)
+        notesListView.adapter = notesAdapter
+
+
+        saveButton.setOnClickListener {
+            val newNote = notesInput.text.toString()
+            if (newNote.isNotEmpty()) {
+                notesList.add(newNote)
+                notesAdapter.notifyDataSetChanged()
+                notesInput.text.clear()
+            }
+
+
+        }
     }
 }
